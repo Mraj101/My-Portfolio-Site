9d502b2a5a98bcfeb9651c830e46dc8426c2bad9
Hasnain <mrajhasnain101@gmail.com>
2023-01-04 22:51:17 +0600

    new file Banner

---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import  Container  from 'react-bootstrap/Container'
+
+import  Row  from 'react-bootstrap/Row'
+const Banner = () => {
+  return (
+    <section className='banner' id='home'>
+     <Row/>
+
+    </section>
+  )
+}
+
+export default Banner
