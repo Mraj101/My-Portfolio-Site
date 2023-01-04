e85241eed3bfa8a74cbc85e366a75e516feffd15
Hasnain <mrajhasnain101@gmail.com>
2023-01-04 23:02:37 +0600

    installed react bootstrap

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import  Container  from 'react-bootstrap/Container'
 
+import  Container  from 'react-bootstrap/Container'
 import  Row  from 'react-bootstrap/Row'
+import  Col  from 'react-bootstrap/Col'
+
+
 const Banner = () => {
   return (
     <section className='banner' id='home'>
-     <Row/>
+        <Container>
+            <Row className='tagline'>
+                <Col xs={12} md={6}>
+                    <span>Welcome to my portfolio</span>
+                    <h1>{`Hasnain Ahmed Miraj`} <span>Web Dev</span></h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, nesciunt.</p>
+                    {/* <button onClick={()=>{}}></button> */}
+                </Col>
+            </Row>
+        </Container>
 
     </section>
   )
