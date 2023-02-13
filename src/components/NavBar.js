2b3ec7d1af8ba16ff9afd1e2ba645b9fe277d106
Hasnain <mrajhasnain101@gmail.com>
2023-02-13 21:56:29 +0600

    edited

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,6 +8,7 @@ import Nav from 'react-bootstrap/Nav'
 import navIcon1 from '../assets/img/nav-icon1.svg'
 import navIcon2 from '../assets/img/nav-icon2.svg'
 import navIcon3 from '../assets/img/nav-icon3.svg'
+import gitIcon from '../assets/img/iconmonstr-github-1.svg'
 import logo from "../assets/img/logo.svg"
 
 const NavBar = () => {
@@ -51,7 +52,7 @@ const NavBar = () => {
             <div className="social-icon">
                 <a href=""><img src={navIcon1} alt="" /></a>
                 <a href=""><img src={navIcon2} alt="" /></a>
-                <a href=""><img src={navIcon3} alt="" /></a>
+                <a href=""><img src={gitIcon} alt="" /></a>
             </div>
             <button className='vvd' onClick={()=>{console.log('connect')}}>Lets Connect</button>
           </span>
