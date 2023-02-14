a34f68a2f2204b00f194634b4183a8ae4cfedf1b
Hasnain <mrajhasnain101@gmail.com>
2023-02-14 21:43:07 +0600

    edeited banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,21 +3,63 @@ import  Container  from 'react-bootstrap/Container'
 import  Row  from 'react-bootstrap/Row'
 import  Col  from 'react-bootstrap/Col'
 import { ArrowRightCircle } from 'react-bootstrap-icons'
-import headerImg from "../assets/img/header-img.svg"
+// import headerImg from "../assets/img/header-img.svg"
 import mirajIMG from "../assets/img/miraj.jpg"
+
+import { useState,useEffect } from 'react'
+
 
 
 
 const Banner = () => {
+  const [loop,setLoop]=useState(0);
+  const [delta,setDelta]=useState(100-(Math.random()*20%90));
+  const [text,setText]=useState('')
+  const [isDeleting,setIsDeleting]=useState(false);
+
+  const rotateText=['welcome','Web Dev','Software Engineer'];
+  let period=2000
+  useEffect(()=>{
+    let ticker=setInterval(() => {
+      tick();
+    }, delta);
+
+    return ()=>{clearInterval(ticker)};
+
+  },[text])
+
+  const tick=()=>{
+     let i=loop%rotateText.length;
+     let fullText=rotateText[i];
+     let updatedText=isDeleting?fullText.substring(0,text.length-1):fullText.substring(0,text.length+1);
+
+     setText(updatedText);
+
+     if(isDeleting){
+        setDelta((prevDelta)=>prevDelta/2.3);
+     }
+     if(!isDeleting && updatedText === fullText){
+      setIsDeleting(true);
+      setDelta(period);
+     }
+     else if(isDeleting && updatedText === '')
+     {
+      setIsDeleting(false);
+      setLoop(loop+1);
+      setDelta(100)
+     }
+
+  }
   return (
     <section className='banner' id='home'>
         <Container>
             <Row className='align-items-center'>
                 <Col xs={12} md={6} xl={7}>
-                    <span className='tagline'>Welcome to my portfolio</span>
-                    <h1>{`Hasnain Ahmed Miraj`} <span>Web Dev</span></h1>
+                    <span className='tagline'>Welcome to my portfolio</span> 
+                    <h1>{`Hasnain Ahmed Miraj`}<span className='h2'>        {text}</span></h1>
+                    <br />
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, nesciunt.</p>
-                    <button onClick={()=>{console.log("hi")}}>Lets connect</button>
+                    <button onClick={()=>{console.log("hi")}}>Lets connect<ArrowRightCircle></ArrowRightCircle></button>
                 </Col>
                 <Col xs={12} md={6} xl={5}>
                         <img src={mirajIMG} alt="header img" />
