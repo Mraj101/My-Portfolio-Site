a564ae138283ab8fb9581404992a7d0b439409f7
Hasnain <mrajhasnain101@gmail.com>
2023-03-07 16:22:35 +0600

    new commit

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,6 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
+import { Circle } from "react-circle";
+
+
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import colorSharp from "../assets/img/color-sharp.png"
@@ -8,7 +8,6 @@ import colorSharp from "../assets/img/color-sharp.png"
 export const Skills = () => {
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5
     },
@@ -36,27 +35,31 @@ export const Skills = () => {
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
-                                <img src={meter1} alt="Meter's" />
-                                <h5>Web Development</h5>
+                            <Circle progress={80} lineWidth={28} bgColor="whiteSmoke"progressColor="hotpink" />
+                                <h5>React</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Meter's" />
-                                <h5>Brand Identity</h5>
+                            <Circle progress={95} lineWidth={28} bgColor="whiteSmoke"progressColor="green"/>
+                                <h5>HTML</h5>
                             </div>
                             <div className="item">
-                                <img src={meter3} alt="Meter's" />
-                                <h5>Logo Design</h5>
+                            <Circle progress={90} lineWidth={28} bgColor="whiteSmoke"progressColor="yellow"/>
+                                <h5>CSS</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Meter's" />
-                                <h5>Web Development</h5>
+                            <Circle progress={85} lineWidth={28} bgColor="whiteSmoke"progressColor="magenta"/>
+                                <h5>python</h5>
+                            </div>
+                            <div className="item">
+                            <Circle progress={85} lineWidth={28} bgColor="whiteSmoke"progressColor="blue"/>
+                                <h5>C++</h5>
                             </div>
                         </Carousel>
                     </div>
                 </div>
             </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="Image" />
+        <img className="background-image-left" src={colorSharp} alt="sharp" />
     </section>
   )
 }
