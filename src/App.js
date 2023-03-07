cce4801f223e3ac27bcf8154fd5d815d3c09051e
Hasnain <mrajhasnain101@gmail.com>
2023-03-07 16:22:11 +0600

    new

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Circle } from 'react-circle';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -6,7 +8,7 @@ import Banner from './components/Banner';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
-import SkillProgressBar from './components/SkillProgressBar';
+
 
 function App() {
   return (
@@ -16,7 +18,7 @@ function App() {
       <Skills/>
       <Projects/>
       <Contact/>
-      <SkillProgressBar bar={75}/>
+      <Circle progress={35}/>
     </div>
   );
 }
