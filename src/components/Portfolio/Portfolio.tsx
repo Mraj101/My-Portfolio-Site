369e7d7b247fb4f6c971fe4cecf930a5e4d84f2d
Hasnain <mrajhasnain101@gmail.com>
2023-04-04 23:12:41 +0600

    all

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -135,14 +135,14 @@ export function Portfolio(){
             <header>
             <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
             <div className="project-links">
-              <a href="https://github.com/Mraj101/ReactMovieDb" target="_blank" rel="noreferrer">
+              <a href="https://github.com/Mraj101/Django-SPORTS-MANAGER" target="_blank" rel="noreferrer">
                 <img src={githubIcon} alt="GitHub" />
               </a>
             </div>
             </header>
             <div className="body">
-              <h3>React Movie Database</h3>
-              <p>Your React web app with TMDB API lets users easily browse popular movies, vote on favorites, and view info like release dates and ratings. It's a user-friendly tool for discovering new and upcoming releases.
+              <h3>Sports Manager App</h3>
+              <p>This is a web app based on python's DJANGO framework, where we can automate sporting system like , player registration, match fixtures, match results, and many more
                 <br />
                 [under developement]
               </p>
