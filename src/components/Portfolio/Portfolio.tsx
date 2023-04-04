606b533b65b7b65bd62e3c132b16db716a788c44
Hasnain <mrajhasnain101@gmail.com>
2023-04-04 23:03:04 +0600

    listings

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -12,44 +12,21 @@ export function Portfolio(){
       <div className="projects">
 
       <ScrollAnimation animateIn="flipInX">
-        <div className="project">
-          <header>
-          <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-          <div className="project-links">
-            <a href="https://hasnains-todo-app.netlify.app/" target="_blank" rel="noreferrer">
-              <img src={githubIcon} alt="GitHub" />
-            </a>
-          </div>
-          </header>
-          <div className="body">
-            <h3>React To Do app</h3>
-            <p>A web application created as a personal project. This todo app allows users to create and manage their to-do lists. It was built using HTML, CSS, and JavaScript and showcases my skills in front-end development.</p>
-          </div>
-          <footer>
-            <ul className="tech-list">
-              <li>react</li>
-              <li>css</li>
-            </ul>
-          </footer>
-        </div>
-      </ScrollAnimation>
-
-        <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
             <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
             <div className="project-links">
-              <a href="https://github.com/joaotuliojt/github-search" target="_blank" rel="noreferrer">
+              <a href="https://github.com/Mraj101/react-todo-app" target="_blank" rel="noreferrer">
                 <img src={githubIcon} alt="GitHub" />
               </a>
-              <a href="https://github-search-blond.vercel.app/" target="_blank" rel="noreferrer">
+              <a href="https://hasnains-todo-app.netlify.app/" target="_blank" rel="noreferrer">
                 <img src={externalLinkIcon} alt="Visitar site" />
               </a>
             </div>
             </header>
             <div className="body">
-              <h3>Countries Searcher</h3>
-              <p>This app allows you to search for country information based on your input. You can view country flags, names, populations, and other relevant data to gain a better understanding of various countries.
+              <h3>React TODO APP</h3>
+              <p>A web application created as a personal project. This todo app allows users to create and manage their to-do lists. It was built using HTML, CSS, and JavaScript in react and showcases my skills in front-end development.
               </p>
             </div>
             <footer>
@@ -62,102 +39,119 @@ export function Portfolio(){
           </div>
         </ScrollAnimation>
 
-        <ScrollAnimation animateIn="flipInX">
-          <div className="project">
-            <header>
-            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-            <div className="project-links">
-              <a href="https://joaotuliojt.github.io/sunnyside-agency/" target="_blank" rel="noreferrer">
-                <img src={externalLinkIcon} alt="Visitar site" />
-              </a>
-            </div>
-            </header>
-            <div className="body">
-              <h3>Sunnyside agency</h3>
-              <p>Uma lading page de uma empresa fictícia proposta pelo <a href="https://www.frontendmentor.io/">Frontend Mentor.</a>
-              </p>
-            </div>
-            <footer>
-              <ul className="tech-list">
-                <li>Html</li>
-                <li>Css</li>
-                <li>JavaScript</li>
-              </ul>
-            </footer>
-          </div>
-        </ScrollAnimation>
+
+        
+
 
         <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
             <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
             <div className="project-links">
-              <a href="https://joaotulio.vercel.app/" target="_blank" rel="noreferrer">
+              <a href="https://github.com/Mraj101/react-Contries-app" target="_blank" rel="noreferrer">
+                <img src={githubIcon} alt="GitHub" />
+              </a>
+              <a href="https://allcountries-information.netlify.app/" target="_blank" rel="noreferrer">
                 <img src={externalLinkIcon} alt="Visitar site" />
               </a>
             </div>
             </header>
             <div className="body">
-              <h3>Portfólio V1</h3>
-              <p>Uma versão alternativa de meu portfólio, utilizando VueJs.
+              <h3>Countries</h3>
+              <p>This app allows you to search for country information based on your input. You can view country flags, names, populations, and other relevant data to gain a better understanding of various countries.
               </p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li>VueJs</li>
-                <li>JavaScript</li>
+                <li>React</li>
+                <li>javascript</li>
+                <li>css</li>
               </ul>
             </footer>
           </div>
         </ScrollAnimation>
+
+        
 
         <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
             <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
             <div className="project-links">
-              <a href="https://github.com/joaotuliojt/dtmoney-trilha-reactjs" target="_blank" rel="noreferrer">
-                <img src={githubIcon} alt="Visitar site" />
+              <a href="https://github.com/Mraj101/ReactMovieDb" target="_blank" rel="noreferrer">
+                <img src={githubIcon} alt="GitHub" />
               </a>
             </div>
             </header>
             <div className="body">
-              <h3>Dtmoney</h3>
-              <p>Um gerenciador de finanças para analisar seus ganhos, gastos e ter um controle maior de suas finanças.
+              <h3>React Movie Database</h3>
+              <p>Your React web app with TMDB API lets users easily browse popular movies, vote on favorites, and view info like release dates and ratings. It's a user-friendly tool for discovering new and upcoming releases.
+                <br />
+                [under developement]
               </p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li>VueJs</li>
-                <li>JavaScript</li>
+                <li>React</li>
+                <li>javascript</li>
+                <li>css</li>
               </ul>
             </footer>
           </div>
         </ScrollAnimation>
+
 
         <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
             <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
             <div className="project-links">
-              <a href="https://github.com/joaotuliojt/dv-musica-library-page" target="_blank" rel="noreferrer">
-                <img src={githubIcon} alt="Visitar site" />
-              </a>
-              <a href="https://joaotuliojt.github.io/dv-musica-library-page/" target="_blank" rel="noreferrer">
+              <a href="https://allcountries-information.netlify.app/" target="_blank" rel="noreferrer">
                 <img src={externalLinkIcon} alt="Visitar site" />
               </a>
             </div>
+            
             </header>
+            
             <div className="body">
-              <h3>Music Library</h3>
-              <p>Uma interface para uma biblioteca de músicas, semelhante ao Spotify.
+              <h3>PortFolio</h3>
+              <p>My React portfolio site is a clean and simple showcase of your work and skills. It features a bio, project list, and contact form for visitors. It effectively presents your capabilities and is easy to navigate.
               </p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li>Html</li>
-                <li>Css</li>
-                <li>JavaSript</li>
+                <li>React</li>
+                <li>typescript</li>
+                <li>css</li>
+                <li>styled components</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+            <div className="project-links">
+              <a href="https://github.com/Mraj101/ReactMovieDb" target="_blank" rel="noreferrer">
+                <img src={githubIcon} alt="GitHub" />
+              </a>
+            </div>
+            </header>
+            <div className="body">
+              <h3>React Movie Database</h3>
+              <p>Your React web app with TMDB API lets users easily browse popular movies, vote on favorites, and view info like release dates and ratings. It's a user-friendly tool for discovering new and upcoming releases.
+                <br />
+                [under developement]
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>React</li>
+                <li>javascript</li>
+                <li>css</li>
               </ul>
             </footer>
           </div>
